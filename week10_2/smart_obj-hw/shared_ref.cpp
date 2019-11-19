0a08a903f90ca50876228615681dd3accb2d9a8d
songheewon <songheew1020@gmail.com>
2019-11-19 03:21:35 -0800

    week10_2

---
+++ week10_2/smart_obj-hw/shared_ref.cpp
@@ -0,0 +1,121 @@
+//
+// Created by 김혁진 on 14/10/2019.
+//
+
+#include "shared_ref.h"
+
+namespace ptr {
+shared_ref::shared_ref() {
+	this->_mgr = empty_mgr;
+    // TODO: Fill it
+    // _mgr의 default 값을 빈 mgr으로 설정
+}
+
+shared_ref::shared_ref(student* student) {
+	if(ptr_map[student] != nullptr) {
+		this->_mgr = empty_mgr;
+	}
+	else {
+		this->_mgr = new mgr(student);
+		ptr_map[student] = this->_mgr;
+	}
+    // TODO: Fill it
+    // ptr_map에 student의 주소가 있으면 _mgr을 빈 mgr으로 설정
+    // ptr_map에 student의 주소가 없으면 _mgr을 student의 메모리를 관리하는 mgr으로 설정하고
+    // ptr_map에 key를 student의 주소로 하고 value를 mgr로 등록 ==> student의 주소가 shared_ref의 mgr로 관리되는지 확인 가능
+}
+
+shared_ref::shared_ref(const shared_ref& shared) {
+	this->_mgr = shared._mgr;
+	increase();
+    // TODO: Fill it
+    // shared_ref 의 _mgr을 자신의 _mgr로 설정
+    // reference가 증가하였으므로 increase 호출
+}
+
+shared_ref::~shared_ref() {
+	release();
+    // TODO: Fill it
+    // mgr을 반환하는 release를 호출하게 만듦
+}
+
+void shared_ref::release() {
+	if(this->_mgr != nullptr) {
+		this->_mgr->count--;
+		if(this->_mgr->count == 0) {
+			ptr_map[this->_mgr->ptr] = nullptr;
+			delete(this->_mgr);
+		}
+		this->_mgr = new mgr();
+	}
+    // TODO: Fill it
+    // _mgr이 nullptr일 경우 아무 작업도 하지 않음
+    // _mgr이 nullptr가 아닐 경우 _mgr의 count를 감소시키고
+    // count가 0인 경우 ptr_map에서 _mgr의 ptr가 key인 value를 nullptr로 설정하고 _mgr을 delete함
+    // ==> student 포인터에 대해 mgr 관리가 끝났음을 알려줌
+}
+
+student *shared_ref::get() const {
+	if(this->_mgr == nullptr) {
+		return nullptr;
+	}
+	else {
+		return this->_mgr->ptr;
+	}
+    // TODO: Fill it
+    // _mgr이 nullptr일 경우 nullptr 반환하고
+    // _mgr이 nullptr가 아닐 경우 _mgr의 ptr을 return 하도록 구현
+}
+
+void shared_ref::increase() {
+	if(this->_mgr != nullptr) {
+		this->_mgr->count++;
+	}
+    // TODO: Fill it
+    // _mgr이 nullptr일 경우 아무런 작업도 하지 않음
+    // _mgr이 nullptr가 아닐 경우 _mgr의 count를 증가시킴
+}
+
+shared_ref &shared_ref::operator=(const shared_ref &r) {
+	if(this == &r) {
+		return *this;
+	}
+	else {
+		release();
+		this->_mgr = r._mgr;
+		increase();
+	}
+    // TODO: Fill it
+    // r의 주소가 this와 같은 경우 아무런 작업도 하지 않고 *this를 리턴
+    // ex: a = a;
+    // r의 주소가 this와 다른 경우 현재 _mgr을 release하고 r의 _mgr을 this의 _mgr로 설정. 그 후 increase
+    // ex: b = a; -> a의 값을 가리키는 것이 a와 b 두 값이 됨
+return *this;
+}
+
+student *shared_ref::operator->() {
+	if(this->_mgr == nullptr) {
+		return nullptr;
+	}
+	else {
+		return this->_mgr->ptr;
+	}
+    // TODO: Fill it
+    // _mgr이 nullptr일 경우 nullptr 반환
+    // _mgr이 nullptr가 아닐 경우 _mgr의 ptr을 반환
+}
+
+int shared_ref::count() {
+	if(this->_mgr == nullptr) {
+		return 0;
+	}
+	else {
+		return this->_mgr->count;
+	}
+    // TODO: Fill it
+    // _mgr이 nullptr일 경우 0을 반환
+    // _mgr이 nullptr가 아닐 경우 _mgr의 count를 반환
+
+}
+
+}
---
+++ week10_2/smart_obj-hw/weak_pointer.cpp
@@ -0,0 +1,54 @@
+//
+// Created by 김혁진 on 2019/10/28.
+//
+
+#include "weak_pointer.h"
+
+namespace ptr {
+ptr::weak_pointer::weak_pointer() {
+	this->ptr = nullptr;
+    // TODO: Fill it
+    // ptr을 default값으로 nullptr로 설정
+}
+
+weak_pointer::weak_pointer(shared_ref &shared) {
+	this->ptr = shared.get();
+    // TODO: Fill it
+    // shared의 get으로 student* 값을 ptr의 값으로 설정
+}
+
+weak_pointer::weak_pointer(const weak_pointer &weak) {
+	this->ptr = weak.ptr;
+    // TODO: Fill it
+    // weak의 ptr을 ptr값으로 설정
+}
+
+int weak_pointer::is_expired() {
+	if(ptr_map[ptr] != nullptr) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+    // TODO: Fill it
+    // ptr_map에 자신의 ptr 값이 있는지 확인하고
+    // ptr_map의 value인 mgr이 해제되었는지 확인
+    // ptr_map에서 ptr을 관리하는 mgr이 있으면(nullptr이 아니면) 1 리턴
+    // 아닐 경우 0 리턴
+
+    return 0;
+}
+
+student *weak_pointer::get() {
+	if(ptr_map[this->ptr] != nullptr) {
+		return ptr;
+	}
+	else {
+		return nullptr;
+	}
+    // TODO: Fill it
+    // ptr_map에서 ptr을 관리하는 mgr이 있으면(nullptr이 아니면) ptr을 리턴
+    // 아닌 경우 nullptr 리턴
+}
+
+}
